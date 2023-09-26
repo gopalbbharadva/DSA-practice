27c5f4993ff36c7c8108c9d0fbf7e450f674d7e0
gopalbbharadva <gopalbbharadva@gmail.com>
2023-09-27 00:18:34 +0530

    Feat: find the array is sorted or not problem covered.

--- Array/1dArray.js
+++ Array/1dArray.js
@@ -19,3 +19,48 @@ console.log(res, 'res')
 
 // Time Complexity: O(N)
 // Space Complexity: O(1)
+
+// ********************************************************************************************
+
+// Check if an Array is Sorted
+// PROBLEM: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing
+// / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
+
+const sampleArr = [1, 2, 3, 4, 5]
+
+// Approach: Brute Force
+const isArraySorted = () => {
+  let isSorted = true
+  for (let i = 0; i < sampleArr.length; i++) {
+    for (let j = i + 1; j < sampleArr.length - 1; j++) {
+      if (arr[i] < arr[j]) {
+        isSorted = false
+      }
+    }
+  }
+  return isSorted
+}
+
+// Complexity Analysis
+
+// Time Complexity: O(N^2)
+// Space Complexity: O(1)
+
+// Approach: Efficient (Single traversal)
+const isArraySortedWithLessTimeComplexity = () => {
+  let isSorted = true
+  let j = 1
+  for (let i = 0; i < sampleArr.length - 1; i++) {
+    if (sampleArr[i] < sampleArr[j]) break
+    isSorted = false
+    j++
+  }
+  return isSorted
+}
+
+// Complexity Analysis
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+// console.log(isArraySorted(), 'isArraySorted')
